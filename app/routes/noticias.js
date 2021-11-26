e200dd3ce5a7f888bc5de9fb61ea7dfc6471ee59
Rushmerk <57403682+pedroperegrinaa@users.noreply.github.com>
2021-11-26 19:25:39 -0300

    modulo 05 finalizado

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,14 @@
+var dbConnection = require('../../config/db_connection')
+
 module.exports = function(app) {
+
+    var connection = dbConnection()
+
     console.log("noticias")
     app.get('/tech', function(req, res) {
-        res.render("noticias/tech");
+        connection.query('select * from noticias', function(error, result) {
+
+            res.render('noticias/tech', { noticias: result });
+        })
     })
 }
